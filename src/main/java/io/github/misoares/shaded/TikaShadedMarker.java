69f5e1b968966146fc91b89411ef73a1a1e225c7
Miguel Soares <mssoares5@gmail.com>
2026-03-05 10:04:27 +0100

    initial

---
+++ src/main/java/io/github/misoares/shaded/TikaShadedMarker.java
@@ -0,0 +1,10 @@
+package io.github.misoares.shaded;
+
+/**
+ * Marker type so this artifact can publish non-empty sources and javadocs jars.
+ */
+public final class TikaShadedMarker {
+  private TikaShadedMarker() {
+    // Utility class.
+  }
+}
